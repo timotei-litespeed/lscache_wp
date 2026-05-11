f89089b64a50640117c69c70a54ec6f42a191a98
Tim LSC <timotei.litespeed@gmail.com>
2026-05-11 15:38:51 +0300

    Fix Classic Widgets edit ESI data

--- src/core.cls.php
+++ src/core.cls.php
@@ -198,6 +198,11 @@ class Core extends Root {
 			$this->cls( 'Admin' );
 		}
 
+		if ( $this->cls( 'Router' )->esi_enabled() ) {
+			add_action( 'in_widget_form', [ $this->cls( 'Admin_Display' ), 'show_widget_edit' ], 100, 3 );
+			add_filter( 'widget_update_callback', __NAMESPACE__ . '\Admin_Settings::validate_widget_save', 10, 4 );
+		}
+
 		if ( defined( 'LITESPEED_DISABLE_ALL' ) && LITESPEED_DISABLE_ALL ) {
 			Debug2::debug( '[Core] Bypassed due to debug disable all setting' );
 			return;
